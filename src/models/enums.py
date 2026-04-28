1d546bf70c78abc9f50eda9ae6417fffe6a37dad
Juliana Cavalcanti <jcav@equinor.com>
2026-04-28 04:25:40 +0200

    feat: Support for Collection and CollectionItems
    Include models, mappers and tabledefs for collections (isets) and
    collctionitems( iset data objects)

--- src/models/enums.py
+++ src/models/enums.py
@@ -5,3 +5,28 @@ class SourceSystem(str, Enum):
     OPENWORKS = "OpenWorks R5000"
     PETREL = "Petrel Studio"
 
+
+class InterpretationDataType(str, Enum):
+    SURFACE_GRID = "SurfaceGrid"
+    HORIZON = "Horizon"
+    POLYGON = "Polygon"
+    FAULT = "Fault"
+    POINT_SET = "PointSet"
+    OTHERS = "Others"
+
+
+class UpdateType(str, Enum):
+    OBJECT_CREATE = "ObjectCreate"
+    OBJECT_UPDATE = "ObjectUpdate"
+    OBJECT_DELETE = "ObjectDelete"
+    COLLECTION_INSERT = "CollectionInsert"
+    COLLECTION_DELETE = "CollectionDelete"
+
+
+class OWDataType(str, Enum):
+    MAP2D = "Map2D"
+    RGRID = "Rgrid"
+    HORIZON3D = "HorizonAttributeCatalog"
+    POLYGON_SET = "MappingPolySet"
+    POINT_SET = "PointSet"
+    OTHERS = "Others"
