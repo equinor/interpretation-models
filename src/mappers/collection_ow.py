dea05801c2ba544484c221a13c2072f0eac3534e
Juliana Cavalcanti <jcav@equinor.com>
2026-05-11 09:41:17 +0200

    fix: alternate CI source_id is data_object_id is null
    if object_id (which is optional) is null, use data_key (which is mandatory)
    as the source_id for a collection item

--- src/mappers/collection_ow.py
+++ src/mappers/collection_ow.py
@@ -129,11 +129,12 @@ def map_collection_item(
         ow_data_object.update_date = ow_data_object.create_date
         ow_data_object.update_user_id = ow_data_object.create_user_id
 
+    ow_id: str = str(ow_data_object.data_object_id) if ow_data_object.data_object_id else ow_data_object.data_key
     source_metadata = SourceMetadata(
         system=SourceSystem.OPENWORKS,
         database=source_context.database,
         project=source_context.project,
-        id=str(ow_data_object.data_object_id) if ow_data_object.data_object_id else None,
+        id=ow_id,
         name=ow_data_object.data_object_name,
         remark=ow_data_object.remark,
         create_user=ow_data_object.create_user_id,
